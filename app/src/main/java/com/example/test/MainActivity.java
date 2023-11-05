093543a93e9f0882d5b6f917279daec8c3ea24b0
Ali <aliburnscooper@gmail.com>
2023-11-05 17:38:46 -0500

    Toolbar stuff is starting to work!

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -2,40 +2,28 @@ package com.example.test;
 
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
-    TestObject test = new TestObject();
-    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button);
-        test.setWords("Hello");
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                FragmentManager manager = getSupportFragmentManager();
-                FragmentTransaction transaction = manager.beginTransaction();
-
-                Bundle bundle = new Bundle();
-                String myWords = test.getWords();
-                bundle.putString("key ", "myWords");
-
-                DetailsFragment detailsFragment = new DetailsFragment();
-                detailsFragment.setArguments(bundle);
-                transaction.replace(R.id.frame, detailsFragment).commit();
-            }
-        });
+        setSupportActionBar(toolbar);
     }//onCreate
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }//onCreateOptionsMenu
 }
