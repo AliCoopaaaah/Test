538ccc841a527a857c1f15cb38e5028ce562b0ed
Ali <aliburnscooper@gmail.com>
2023-11-07 22:35:33 -0500

    IT WORKS??!

--- app/src/main/java/com/example/test/DadJoke.java
+++ app/src/main/java/com/example/test/DadJoke.java
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DadJoke extends Fragment {
@@ -14,7 +15,18 @@ public class DadJoke extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_dad_joke, container, false);
+        View view = inflater.inflate(R.layout.fragment_dad_joke, container, false);
 
+        TextView answer = (TextView) view.findViewById(R.id.textView2);
+        Button button = (Button) view.findViewById(R.id.answer);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answer.setText(getResources().getString(R.string.answer));
+            }
+        });
+
+        return view;
     }
 }
