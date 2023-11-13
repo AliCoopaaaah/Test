a4818b69e7788c6ed4d961b9288bf27d803ce1c9
Ali <aliburnscooper@gmail.com>
2023-11-12 21:24:56 -0500

    Trying to recreate battle.net app

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -7,8 +7,13 @@ import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -26,6 +31,15 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ListView list = findViewById(R.id.friendsList);
+
+        ArrayList<String> usernames = new ArrayList<String>();
+        usernames.add("Friend 1");
+        usernames.add("Friend 2");
+        usernames.add("Friend 3");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_layout, usernames);
+        list.setAdapter(adapter);
     }//onCreate
 
 }
