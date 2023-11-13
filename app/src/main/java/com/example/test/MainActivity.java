2c78694a305da9e6f501f777bbb03d39aebf7fe3
Ali <aliburnscooper@gmail.com>
2023-11-12 22:29:05 -0500

    additional testing required

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -23,23 +23,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView icon = findViewById(R.id.icon);
-        icon.setImageResource(R.drawable.img);
-
-        Button friends = findViewById(R.id.friends);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        ListView list = findViewById(R.id.friendsList);
-
-        ArrayList<String> usernames = new ArrayList<String>();
-        usernames.add("Friend 1");
-        usernames.add("Friend 2");
-        usernames.add("Friend 3");
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_layout, usernames);
-        list.setAdapter(adapter);
     }//onCreate
 
 }
