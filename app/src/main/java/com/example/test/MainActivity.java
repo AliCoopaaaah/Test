fded5de25a775a00203f2a2498e920d3ce49a999
Ali <aliburnscooper@gmail.com>
2023-11-27 19:26:13 -0500

    Adding new activity

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,18 +1,31 @@
 package com.example.test;
 
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.DatePickerDialog;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,27 +33,87 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-    Toolbar toolbar;
     ImageView image;
     Bitmap bitmap;
+
     InputStream input1;
-    String date;
+    private ArrayList list;
+
+    //dates to be appended to the URL
+    int URLyear = 1996;
+    int URLmonth = 3;
+    int URLday = 25;
+    String URLdate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        image = findViewById(R.id.image);
-        toolbar = findViewById(R.id.toolbar);
-
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        NASAImages nasa = new NASAImages();
-        nasa.execute();
+        TextView datePicked = (TextView) findViewById(R.id.datePicked);
+        Button dateButton = (Button) findViewById(R.id.dateButton);
+        Button show = (Button) findViewById(R.id.show);
+        image = (ImageView) findViewById(R.id.image);
+
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        MainActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int thisyear, int monthOfYear, int dayOfMonth) {
+                                URLyear = thisyear;
+                                URLmonth = monthOfYear;
+                                URLday = dayOfMonth;
+                                datePicked.setText(thisyear+"-"+(monthOfYear+1)+"-"+dayOfMonth);
+                            }
+                        },
+                        year, month, day);
+                datePickerDialog.show();
+            }
+        });
+/*
+
+*/
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                URLdate = (URLyear+"-"+(URLmonth+1)+"-"+URLday).toString();
+                NASAImages nasa = new NASAImages();
+                nasa.execute();
+            }
+        });
     }//onCreate
 
+    //-------------------------Saved Instances-------------------------
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putParcelableArrayList("list", list);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+       list = savedInstanceState.getParcelableArrayList("list");
+       super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    //-------------------------Menu Options-------------------------
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -48,10 +121,17 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }//onCreateOptionsMenu
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        return true;
+    }//onOptionsItemSelected
+
+    //-------------------------URL Connection and AsyncTask-------------------------
     public void URLConnection() throws IOException {
         HttpURLConnection connection;
         //change the date to a date variable
-        String url = "https://api.nasa.gov/planetary/apod?api_key=hm97X7cDaP9hZN1E1oGdbS7QVSJbI3W4jdzTzKva&date=2020-02-01";
+        String url = "https://api.nasa.gov/planetary/apod?api_key=hm97X7cDaP9hZN1E1oGdbS7QVSJbI3W4jdzTzKva&date="+URLdate;
         connection = (HttpURLConnection) new URL(url).openConnection();
         connection.connect();
         input1 = connection.getInputStream();
@@ -95,7 +175,6 @@ public class MainActivity extends AppCompatActivity {
             if(image != null){
                 image.setImageBitmap(bitmap);
             }
-            new NASAImages().execute();
         }
     }//NASAImages
 }
